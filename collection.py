ada951c989a44d35c306d1c9d395f1b79d6e43fc
ethilty <ethilty@crimson.ua.edu>
2021-10-31 02:29:47 -0500

    set_all fix

--- collection.py
+++ collection.py
@@ -10,12 +10,14 @@ def collection():
 @click.argument('key')
 @click.argument('value')
 def set_all(path, key, value):
-    with open(path, 'w+') as file:
+    with open(path, 'r+') as file:
         data = json.load(file)
         # Set key of all poems
         for poem_data in data:
             poem_data[key] = value
+        file.seek(0)
         json.dump(data, file)
+        file.truncate()
 
 if __name__ == '__main__':
     collection()
