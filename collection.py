48e9a8a6811de5873bae8268e2a9fc5b803fc3e6
ethilty <ethilty@crimson.ua.edu>
2021-10-31 02:53:15 -0500

    removeall command

--- collection.py
+++ collection.py
@@ -16,7 +16,20 @@ def set_all(path, key, value):
         for poem_data in data:
             poem_data[key] = value
         file.seek(0)
-        json.dump(data, file)
+        json.dump(data, file, indent=4)
+        file.truncate()
+
+@collection.command('removeall')
+@click.argument('path', type=click.Path(exists=True))
+@click.argument('key')
+def set_all(path, key):
+    with open(path, 'r+') as file:
+        data = json.load(file)
+        # Remove key of all poems
+        for poem_data in data:
+            del poem_data[key]
+        file.seek(0)
+        json.dump(data, file, indent=4)
         file.truncate()
 
 if __name__ == '__main__':
