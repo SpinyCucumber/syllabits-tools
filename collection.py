e339529fa61db89c742e5f5e342f560787923916
ethilty <spinycucumber@gmail.com>
2021-11-01 22:25:42 -0500

    usetext command

--- collection.py
+++ collection.py
@@ -6,11 +6,11 @@ def collection():
     pass
 
 @collection.command('setall')
-@click.argument('path', type=click.Path(exists=True))
+@click.argument('collection_path', type=click.Path(exists=True))
 @click.argument('key')
 @click.argument('value')
-def set_all(path, key, value):
-    with open(path, 'r+') as file:
+def set_all(collection_path, key, value):
+    with open(collection_path, 'r+') as file:
         data = json.load(file)
         # Set key of all poems
         for poem_data in data:
@@ -18,12 +18,13 @@ def set_all(path, key, value):
         file.seek(0)
         json.dump(data, file, indent=4)
         file.truncate()
+        file.close()
 
 @collection.command('removeall')
-@click.argument('path', type=click.Path(exists=True))
+@click.argument('collection_path', type=click.Path(exists=True))
 @click.argument('key')
-def set_all(path, key):
-    with open(path, 'r+') as file:
+def set_all(collection_path, key):
+    with open(collection_path, 'r+') as file:
         data = json.load(file)
         # Remove key of all poems
         for poem_data in data:
@@ -31,6 +32,34 @@ def set_all(path, key):
         file.seek(0)
         json.dump(data, file, indent=4)
         file.truncate()
+        file.close()
+
+@collection.command('usetext')
+@click.argument('collection_path', type=click.Path(exists=True))
+@click.argument('text_path', type=click.Path(exists=True))
+def use_text(collection_path, text_path):
+    with open(collection_path, 'r+') as collection_file, open(text_path, 'r') as text_file:
+        data = json.load(collection_file)
+
+        poem_index = 0
+        line_index = 0
+        for line in text_file:
+            stripped = line.rstrip()
+            if (stripped):
+                # Copy line into collection
+                data[poem_index]['lines'][line_index]['text'] = stripped
+                line_index += 1
+            else:
+                poem_index += 1
+                line_index = 0
+        
+        # Rewrite collection
+        collection_file.seek(0)
+        json.dump(data, collection_file, indent=4)
+        collection_file.truncate()
+
+        text_file.close()
+        collection_file.close()
 
 if __name__ == '__main__':
     collection()
