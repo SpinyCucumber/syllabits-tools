c72c551bd899340a70abc54279eac2e86fa006d3
ethilty <spinycucumber@gmail.com>
2022-02-06 17:45:28 -0600

    import_workbook command

--- collection.py
+++ collection.py
@@ -1,21 +1,29 @@
 import json
 import click
+import openpyxl
+
+footLookup = {
+    'u/': 'i',
+    '/u': 't',
+    '/uu': 'd',
+    'uu/': 'a',
+    '//': 's',
+    'uu': 'p',
+}
 
 @click.group()
 def collection():
     pass
 
 def collection_transformer(func):
-    @click.argument('path', type=click.Path(exists=True))
-    def wrapped(path, *args, **kwargs):
-        with open(path, 'r+') as file:
-            data = json.load(file)
-            # Transform collection data and write aback to file
-            func(data, *args, **kwargs)
-            file.seek(0)
-            json.dump(data, file)
-            file.truncate()
-            file.close()
+    @click.argument('file', type=click.File('r+'))
+    def wrapped(file, *args, **kwargs):
+        data = json.load(file)
+        # Transform collection data and write aback to file
+        func(data, *args, **kwargs)
+        file.seek(0)
+        json.dump(data, file)
+        file.truncate()
     return wrapped
 
 def poem_transformer(func):
@@ -39,23 +47,43 @@ def set_all(poem, key, value):
 def remove_all(poem, key):
     del poem[key]
 
-@collection.command('usetext')
-@click.argument('text_path', type=click.Path(exists=True))
+@collection.command('useplaintext')
+@click.argument('text_file', type=click.File())
 @collection_transformer
-def use_text(data, text_path):
-    with open(text_path, 'r') as text_file:
-        poem_index = 0
+def use_plaintext(data, text_file):
+    poem_index = 0
+    line_index = 0
+    for line in text_file:
+        stripped = line.rstrip()
+        if (stripped):
+            # Copy line into collection
+            data[poem_index]['lines'][line_index]['text'] = stripped
+            line_index += 1
+        else:
+            poem_index += 1
+            line_index = 0
+
+@collection.command('importworkbook')
+@click.argument('workbook_path', type=click.Path(exists=True))
+@collection_transformer
+def import_workbook(data, workbook_path):
+    # Load workbook and iterate through worksheets
+    workbook = openpyxl.load_workbook(workbook_path)
+    for worksheet in workbook:
+        # Find corresponding poem (worksheet title should be index)
+        poem_index = int(worksheet.title) - 1
+        poem = data[poem_index]
+        # Iterate over pairs of rows. First row is key, second is text
+        rows = worksheet.rows
         line_index = 0
-        for line in text_file:
-            stripped = line.rstrip()
-            if (stripped):
-                # Copy line into collection
-                data[poem_index]['lines'][line_index]['text'] = stripped
-                line_index += 1
-            else:
-                poem_index += 1
-                line_index = 0
-        text_file.close()
+        for key_row, text_row in zip(rows, rows):
+            # Convert rows into actual key and text
+            # We slice the text to omit comments and such
+            line = poem['lines'][line_index]
+            key = [footLookup.get(cell.value, '') for cell in key_row if cell.value != None]
+            line['key'] = key
+            line['text'] = ' '.join([str(cell.value) for cell in text_row[:len(key)]])
+            line_index += 1
 
 @collection.command('splitkey')
 @poem_transformer
